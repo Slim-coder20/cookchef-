b41345f5b12d1d3aff30a53f4332aaedfffb466c
slim <slimabida21@gmail.com>
2025-10-08 17:26:55 +0200

    Ajout du data en dur et mapper la liste depuis le composant content pour afficher les recettes dynamiquement

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,19 +1,18 @@
 import styles from "./Content.module.scss";
 import Recipe from "../recipe/Recipe";
+import { data } from "../../data/recipies";
+import type { Recipe as RecipeType } from "../../data/recipies";
+
 export default function Content() {
+  const recipes: RecipeType[] = data;
   return (
     <div className="flex-fill container p-20">
       <h1 className=" my-30">Découvrez nos nouvelles recettes </h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+          {recipes.map((r) => (
+            <Recipe key={r._id} title={r.title} image={r.image} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/recipe/Recipe.tsx
+++ src/components/recipe/Recipe.tsx
@@ -1,16 +1,20 @@
 import styles from "./Recipe.module.scss";
-import recipe from "../../assets/images/recette.jpg";
 
-export default function Recipe() {
+type Props = {
+  title: string;
+  image: string;
+};
+
+export default function Recipe({ title, image }: Props) {
   return (
     <div className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={recipe} alt="recipe" />
+        <img src={image} alt="recipe" />
       </div>
       <div
         className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
       >
-        <h3>Saumon et asperges</h3>
+        <h3>{title}</h3>
       </div>
     </div>
   );
