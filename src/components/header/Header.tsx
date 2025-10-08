84b942d545f87a3db658b238a18645f29cd6ae7d
slim <slimabida21@gmail.com>
2025-10-09 00:06:03 +0200

    Mise en place du menuburger en version mobile avec la création d'un state useState dans le composant header et la creation d'un nouveau composant HeaderMenu  qui servira comme le menu header en version mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,33 @@
 import styles from "../header/Header.module.scss";
 import cookchef from "../../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "../headerMenu/HeaderMenu";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`d-flex flex-row align-items  ${styles.header}`}>
-      <i class="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
         <img src={cookchef} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i class="fa-solid fa-basket-shopping mr-5"></i>
-          <span>panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="mr-5 btn btn  btn-primary">connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
